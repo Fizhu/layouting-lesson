3547a13f0491f9bd1bad40e42444e70555df1759
Hafizh Anbiya <hvyz.anbiya@gmail.com>
2020-11-16 20:54:33 +0700

    adding alyout counter with linear layout

---
+++ app/src/main/java/com/fizhu/layoutinglesson/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fizhu.layoutinglesson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_counter_linear)
+    }
+}
